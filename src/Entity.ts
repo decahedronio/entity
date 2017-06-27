6918097fa42839e78fcd52c12c483dde2b719fc3
Leo Sjöberg <leo@decahedron.io>
2017-06-28 00:16:57 +0200

    Add support for empty arrays by @Type annotation and support for name aliasing for primitives

--- src/Entity.ts
+++ src/Entity.ts
@@ -14,12 +14,12 @@
             private static jsonParse<T>(sourceObject: T, jsonObject: Object): T {
                 for (let key in jsonObject) {
                     if (jsonObject.hasOwnProperty(key)) {
+                        const metadata: TypeMetadata = defaultMetadataStorage.findTypeMetadata(sourceObject.constructor, key);
                         const value: any = jsonObject[key];
 
                         // We shouldn't copy objects to our entity, as the entity
                         // should be responsible for constructing these itself.
                         if (typeof value === 'object' && !(value instanceof Array)) {
-                            const metadata = defaultMetadataStorage.findTypeMetadata(sourceObject.constructor, key);
                             if (metadata) {
                                 sourceObject[metadata.propertyName] = EntityBuilder.buildOne(metadata.type, value);
                             }
@@ -31,11 +31,18 @@
                         // in which case the entity itself should be assumed
                         // responsible to construct the array of entities.
                         if (value instanceof Array && value.length > 0 && typeof value[0] === 'object') {
-                            const metadata: TypeMetadata = defaultMetadataStorage.findTypeMetadata(sourceObject.constructor, key);
-
                             if (metadata) {
                                 sourceObject[metadata.propertyName] = EntityBuilder.buildMany(metadata.type, value);
                             }
+
+                            continue;
+                        }
+
+                        // Since all other scenarios have been exhausted, we're dealing with a primitive of some form.
+                        // This can be an empty array of objects too, but since it's empty, there's no need for us
+                        // to build an entity. As such, we can just assign it. The same goes for all primitives.
+                        if (metadata) {
+                            sourceObject[metadata.propertyName] = value;
 
                             continue;
                         }
