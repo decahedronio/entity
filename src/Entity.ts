ffd6bd53e9c8d27dd7ce918ca43bd6613509132a
Hakan Aktas <iletisim@hakanaktas.com.tr>
2021-10-01 17:49:52 +0300

    getting rid of TS2536 error

--- src/Entity.ts
+++ src/Entity.ts
@@ -26,7 +26,7 @@ type EntityPropsOnly<T> = {
 
 export class Entity {
 
-    private static async jsonParseAsync<T extends {[key: string]: any}>(sourceObject: T, jsonObject: any): Promise<T> {
+    private static async jsonParseAsync<T extends Entity>(sourceObject: T, jsonObject: Partial<CamelKeysToSnake<EntityPropsOnly<T>>>): Promise<T> {
         const obj = Entity.jsonParse<T>(sourceObject, jsonObject, true);
         for (const key in obj) {
             if (obj.hasOwnProperty(key)) {
@@ -39,74 +39,90 @@ export class Entity {
     /*
      * Parse a generic object into an entity object.
      */
-    private static jsonParse<T extends {[key: string]: any}>(sourceObject: T, jsonObject: any, async = false): T {
-        for (let key in jsonObject) {
-            if (jsonObject.hasOwnProperty(key)) {
-                const metadata: TypeMetadata = defaultMetadataStorage.findTypeMetadata(sourceObject.constructor, key);
-                const value: any = jsonObject[key];
+    private static jsonParse<T extends Entity>(entity: T, data: Partial<CamelKeysToSnake<EntityPropsOnly<T>>>, async = false): T {
+        for (let key in data) {
+            if (!data.hasOwnProperty(key)) {
+                continue;
+            }
 
-                // We shouldn't copy objects to our entity, as the entity
-                // should be responsible for constructing these itself.
-                if (value !== null && typeof value === 'object' && !(value instanceof Array)) {
-                    if (metadata) {
-                        sourceObject[metadata.propertyName] = async
-                            ? EntityBuilder.buildOneAsync(metadata.type, value)
-                            : EntityBuilder.buildOne(metadata.type, value);
-                    }
+            let value = data[key] as any;
 
-                    continue;
-                }
+            const metadata: TypeMetadata = defaultMetadataStorage.findTypeMetadata(entity.constructor, key);
 
-                // if we have an array, we check if it contains objects,
-                // in which case the entity itself should be assumed
-                // responsible to construct the array of entities.
-                if (value instanceof Array && value.length > 0 && typeof value[0] === 'object') {
-                    if (metadata) {
-                        sourceObject[metadata.propertyName] = async
-                            ? EntityBuilder.buildManyAsync(metadata.type, value)
-                            : EntityBuilder.buildMany(metadata.type, value);
-                    }
-
-                    continue;
-                }
-
-                // Since all other scenarios have been exhausted, we're dealing with a primitive of some form.
-                // This can be an empty array of objects too, but since it's empty, there's no need for us
-                // to build an entity. As such, we can just assign it. The same goes for all primitives.
+            // We shouldn't copy objects to our entity, as the entity
+            // should be responsible for constructing these itself.
+            if (value !== null && typeof value === 'object' && !(value instanceof Array)) {
                 if (metadata) {
-                    sourceObject[metadata.propertyName] = value;
-
-                    continue;
+                    entity.setProp(
+                        metadata.propertyName,
+                        async
+                            ? EntityBuilder.buildOneAsync(metadata.type, value)
+                            : EntityBuilder.buildOne(metadata.type, value)
+                    );
                 }
-
-                key = EntityBuilder.enableCamelConversion ? StringHelper.toCamel(key) : key;
-
-                if (sourceObject.hasOwnProperty(key)) {
-                    sourceObject[key] = value;
+                continue;
+            }
+            // if we have an array, we check if it contains objects,
+            // in which case the entity itself should be assumed
+            // responsible to construct the array of entities.
+            if (value instanceof Array && value.length > 0 && typeof value[0] === 'object') {
+                if (metadata) {
+                    entity.setProp(
+                        metadata.propertyName,
+                        async
+                            ? EntityBuilder.buildManyAsync(metadata.type, value)
+                            : EntityBuilder.buildMany(metadata.type, value)
+                    );
                 }
-
-                const defaultValueCallback = defaultMetadataStorage.findCallback(sourceObject.constructor, key);
-                if (defaultValueCallback && defaultValueCallback.condition(sourceObject[key])) {
-                    sourceObject[key] = defaultValueCallback.callback();
-                }
+                continue;
+            }
+            // Since all other scenarios have been exhausted, we're dealing with a primitive of some form.
+            // This can be an empty array of objects too, but since it's empty, there's no need for us
+            // to build an entity. As such, we can just assign it. The same goes for all primitives.
+            if (metadata) {
+                entity.setProp(metadata.propertyName, value);
+                continue;
+            }
+            const newKey = EntityBuilder.enableCamelConversion ? StringHelper.toCamel(key) : key;
+            if (entity.hasOwnProperty(newKey)) {
+                entity.setProp(newKey, value);
+            }
+            const defaultValueCallback = defaultMetadataStorage.findCallback(entity.constructor, newKey);
+            if (defaultValueCallback && defaultValueCallback.condition(entity.getProp(newKey))) {
+                entity.setProp(newKey, defaultValueCallback.callback());
             }
         }
+        return entity as T;
+    }
 
-        return sourceObject;
+    private getProp(key: string) {
+        if (!this.hasOwnProperty(key)) {
+            return;
+        }
+
+        return (this as any)[key];
+    }
+
+    private setProp(key: string, value: any) {
+        if (!this.hasOwnProperty(key)) {
+            return;
+        }
+
+        (this as any)[key] = value;
     }
 
     /*
      * Convert JSON data to an Entity instance.
      */
-    fromJson(jsonData: Partial<CamelKeysToSnake<EntityPropsOnly<this>>>): void {
-        Entity.jsonParse(this, jsonData);
+    fromJson<T extends Entity>(this: T, jsonData: Partial<CamelKeysToSnake<EntityPropsOnly<T>>>): void {
+        Entity.jsonParse<T>(this, jsonData);
     }
 
     /*
      * Convert JSON data to an Entity instance that has async types.
      */
-    async fromJsonAsync(jsonData: any): Promise<void> {
-        await Entity.jsonParseAsync(this, jsonData);
+    async fromJsonAsync<T extends Entity>(this: T, jsonData: Partial<CamelKeysToSnake<EntityPropsOnly<T>>>): Promise<void> {
+        await Entity.jsonParseAsync<T>(this, jsonData);
     }
 
     toJson(toSnake?: true, asString?: false): CamelKeysToSnake<EntityPropsOnly<this>>;
