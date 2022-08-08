f3d07fe9651cd5c8a05dcfceca88feb78bad9368
Hakan Aktas <iletisim@hakanaktas.com.tr>
2022-08-08 16:41:03 +0300

    delete async stuff from circular dep tests

--- spec/CircularDeps/blog.ts
+++ spec/CircularDeps/blog.ts
@@ -1,6 +1,6 @@
 import { Entity } from '../../src/Entity';
 import { Type } from '../../src/support/Type';
-import Comment, { CommentAsync } from './comment';
+import Comment from './comment';
 
 export class BlogPost extends Entity {
     public title: string = null;
@@ -8,20 +8,4 @@ export class BlogPost extends Entity {
 
     @Type(() => require('./comment'))
     public comments: Comment[] = [];
-}
-
-export class BlogPostImport extends Entity {
-    public title: string = null;
-    public body: string = null;
-
-    @Type(() => import('./comment'))
-    public comments: CommentAsync[] = [];
-}
-
-export class BlogPostAsync extends Entity {
-    public title: string = null;
-    public body: string = null;
-
-    @Type(async () => (await import('./comment')).CommentAsync)
-    public comments: CommentAsync[] = [];
 }
